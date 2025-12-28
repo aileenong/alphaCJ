437f46acde2cad9d07add03874bf5f1d2543fd50
aileenong <38133818+aileenong@users.noreply.github.com>
2025-12-28 16:33:00 +0800

    Update solar.py

--- solar.py
+++ solar.py
@@ -428,7 +428,7 @@ if not st.session_state.logged_in:
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     if st.button("Login"):
-        if username == "admin" and password == "1234":
+        if (username == "admin" or username == "Admin") and password == "1234":
             st.session_state.logged_in = True
             st.session_state.menu = "Home"
             st.session_state.username = username
@@ -808,3 +808,4 @@ elif st.session_state.logged_in:
     
     
     
+
